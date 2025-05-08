9d3a41ab01d4b812d1e7ad6a76dea6c172bab506
diegorhcp89 <diegoferreiradoamaral@gmail.com>
2025-05-08 11:41:58 -0300

    Variáveis de Ambiente

---
+++ src/configs/jwt_configs.py
@@ -0,0 +1,7 @@
+import os
+
+jwt_infos = {
+    "KEY": os.getenv("KEY"),
+    "ALGORITH": os.getenv("ALGORITH"),
+    "JWT_HOURS": os.getenv("JWT_HOURS")
+}
--- src/drivers/jwt_handler.py
+++ src/drivers/jwt_handler.py
@@ -1,22 +1,36 @@
 import jwt
 from datetime import datetime, timedelta, timezone
+from src.configs.jwt_configs import jwt_infos
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Carrega as variáveis do arquivo .env
+
+jwt_infos = {
+    "KEY": os.getenv("KEY"),
+    "ALGORITHM": os.getenv("ALGORITHM"),
+    "JWT_HOURS": os.getenv("JWT_HOURS")
+}
+
+print()
+print(jwt_infos)
 
 class JwtHandler:
     def create_jwt_token(self, body: dict = {}) -> str:
         token = jwt.encode(
             payload={
-                'exp': datetime.now(timezone.utc) + timedelta(minutes=1),
+                'exp': datetime.now(timezone.utc) + timedelta(hours=int(jwt_infos["JWT_HOURS"])),
                 **body
             },
-            key="minhaChave",
-            algorithm="HS256"
+            key=jwt_infos["KEY"],
+            algorithm=jwt_infos["ALGORITHM"]
         )
         return token
 
     def decode_jwt_token(self, token: str) -> dict:
         token_information = jwt.decode(
             token,
-            key="minhaChave",
-            algorithms="HS256"
+            key=jwt_infos["KEY"],
+            algorithms=jwt_infos["ALGORITHM"]
         )
         return token_information
